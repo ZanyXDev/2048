d2adf63a86ad7fb723f7a5fc7807589b71b86062
zanyxdev <zanyxdev@gmail.com>
2017-12-23 18:07:55 +0300

    Create first runnable prototip

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,90 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->createGameField();
+    QObject::connect(ui->newGameBtn,SIGNAL(clicked()),this,SLOT(startGame()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::createGameField()
+{
+    for (int x = 0; x < FIELD_SIZE; x++){
+        for (int y = 0; y < FIELD_SIZE; y++){
+            QLabel *item = new QLabel("");
+            item->setAlignment(Qt::AlignCenter);
+            item->setStyleSheet("QLabel { background-color: #cdc1b5; color: black; font:20pt; font-weight:400; border-radius: 5px;}");
+            cells.append(item);
+            ui->gridGame->addWidget(item,x,y);
+        }
+    }
+}
+
+
+// ----------------------- PUBLIC SLOTS -----------------------------------
+void MainWindow::startGame()
+{
+    this->addTile();
+}
+
+void MainWindow::addTile()
+{
+    if ( isFindEmptyCell() ){
+        bool isSuccessSetTile = false;
+        do{
+            int index = getRandomIndex();
+            if (cells.at(index)->text() == "" ){
+                cells.at(index)->setText(getTwoInRandomPow());
+                isSuccessSetTile = true;
+            }
+        } while (!isSuccessSetTile);
+    }
+
+}
+
+
+// ----------------------- Protected  --------------------------------------
+int MainWindow::getRandomIndex()
+{
+    random();
+    int posX = rand() % FIELD_SIZE;
+    int posY = rand() % FIELD_SIZE;
+    return (posX + (posY * FIELD_SIZE));
+}
+
+QString MainWindow::getTwoInRandomPow()
+{
+    random();
+    int new_step = rand() % MAX_POW;
+    if (new_step == 0){
+        new_step = 1;
+    }
+    int value = (int) pow(2,new_step);
+
+    return QString::number(value);
+
+}
+
+void MainWindow::clearGameField()
+{
+    foreach (QLabel *item, cells) {
+        item->setText("");
+        item->setAlignment(Qt::AlignCenter);
+        item->setStyleSheet("QLabel { background-color: #cdc1b5; color: black; font:20pt; font-weight:400; border-radius: 5px;}");
+    }
+}
+
+bool MainWindow::isFindEmptyCell()
+{
+    foreach (QLabel *item, cells) {
+        if (item->text() == ""){
+            return true;
+        }
+    }
+    return false;
+
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,10 +2,19 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QVector>
+#include <QLabel>
+#include <math.h>
 
 namespace Ui {
 class MainWindow;
 }
+
+
+// @TODO replace define to setter and getter. Save values to preference? change from menu
+
+#define FIELD_SIZE 4
+#define MAX_POW 11
 
 class MainWindow : public QMainWindow
 {
@@ -15,8 +24,20 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+public slots:
+    void startGame(); // @TODO add parametr int = size {4,5,6} size of game filed
+
 private:
     Ui::MainWindow *ui;
+    QVector <QLabel *> cells;
+    void createGameField();
+    void addTile();
+
+protected:
+    int getRandomIndex();
+    QString getTwoInRandomPow();
+    void clearGameField();
+    bool isFindEmptyCell();
 };
 
 #endif // MAINWINDOW_H
